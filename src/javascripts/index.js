a724cd39ad1700fec3608c0be754b2002ecd62e0
wendycan <wendycaner@gmail.com>
2016-11-13 10:53:03 +0800

    Can edit content

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,15 +1,28 @@
 require('../stylesheets/index.less');
 
-const md = window.markdownit();
-md.disable(['heading', 'code', 'table', 'blockquote',
-    'hr', 'list', 'link', 'autolink', 'emphasis', 'fence',
-    'lheading', 'escape', 'reference', 'html_block', 'newline', 'backticks']);
-
 class Forsythia {
     constructor(id, options) {
         this.el = document.querySelector(`#${id}`);
         const defaultOptions = {};
         this.options = Object.assign({}, defaultOptions, options);
+        this.content = this.options.content;
+        this.md = window.markdownit();
+        // this.md.disable(['heading', 'code', 'table', 'blockquote',
+        //     'hr', 'list', 'link', 'autolink', 'emphasis', 'fence',
+        //     'lheading', 'escape', 'reference', 'html_block', 'newline', 'backticks']);
+        this.initDom();
+    }
+
+    setContent() {
+        if (this.options.syntax === 'markdown') {
+            this.el.innerHTML = this.md.render(this.content);
+        }
+    }
+
+    initDom() {
+        this.el.className += ' forsythia';
+        this.el.setAttribute('contenteditable', true);
+        this.setContent();
     }
 }
 
